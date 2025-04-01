a99cca0658ed0dd5674231eda33494e7e01a1b94
renato <renatojs.web@gmail.com>
2025-03-31 23:18:19 -0300

    feat(jwt)

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   # /login
   resources :users, only: [:create, :index, :show, :update, :destroy]
   post 'login', to: 'users#login'
+  post 'refresh_token', to: 'users#refresh_token'
 
   get "up" => "rails/health#show", as: :rails_health_check
 
