b46b0c24ee45ef590c0e5a5639f164ee39a24a22
renato <renatojs.web@gmail.com>
2025-03-31 23:24:41 -0300

    docs(insomnia collection)

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,11 @@ Rails.application.routes.draw do
   # end
 
   # /login
+  # post 'users', to: 'users#create'
+  # get 'users', to: 'users#index'
+  # get 'users/:id', to: 'users#show'
+  # patch 'users/:id', to: 'users#update'
+  # delete 'users/:id', to: 'users#destroy'  
   resources :users, only: [:create, :index, :show, :update, :destroy]
   post 'login', to: 'users#login'
   post 'refresh_token', to: 'users#refresh_token'
